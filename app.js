071a392f9d6e1da06d3b42b69f234bdfea674add
Anders Brownworth <anders-git@evantide.com>
2022-01-05 20:49:04 +0000

    basic locking working

--- app.js
+++ app.js
@@ -24,19 +24,66 @@ server.put('/data/:key', async (req, res, next) => {
     const o = await db.get(req.params.key);
 
     if (o) {
-	console.log('PUT', 'key already exists');
-	res.send({error: 'key already exists'});
+	console.log('PUT', req.params.key, 'ERROR: Key already exists!');
+	res.send({error: 'Key already exists!'});
 
     }
     else {
-	const o = db.put(req.params.key, {lockedBy: '', value: req.body});
-	console.log('aaa', o);
+	const tmp = db.put(req.params.key, {lockedBy: '', value: req.body});
 	console.log('PUT', req.params.key, {lockedBy: '', value: req.body});
-	res.send({success: o ? true : false});
+	res.send({success: tmp ? true : false});
 
     }
     return next();
 });
+server.post('/data/:key', async (req, res, next) => {
+    let o = await db.get(req.params.key);
+
+    if (!o) {
+	console.log('POST', req.params.key, 'ERROR: Key doesn\'t exist!');
+	res.send({error: 'Key doesn\'t exist!'});
+	return next();
+    }
+
+    let obj, err;
+    try {
+	obj = JSON.parse(req.body);
+    }
+    catch (e) {
+	err = 'JSON parse error';
+    }
+
+    if (err) {
+	console.log('POST', req.params.key, err);
+	res.send({success: false, reason: 'JSON parse error'});
+	return next();
+    }
+
+    if (obj.user) {
+	if (obj.command == 'lock') {
+	    if (o.lockedBy == '' || o.lockedBy == obj.user) {
+		o.lockedBy = obj.user;
+		db.put(req.params.key, o);
+		console.log('POST', req.params.key, obj.command, 'success');
+		res.send({success: true});
+	    }
+	    else {
+		console.log('POST', req.params.key, obj.command, 'key is locked', obj);
+                res.send({success: false, reason: 'Key is already locked.'});
+	    }
+	}
+	else {
+	    console.log('POST', req.params.key, obj.command, 'command unsupported');
+	    res.send({success: false, reason: 'command unsupported'});
+	}
+    }
+    else {
+	console.log('POST', req.params.key, obj.command, 'no user given');
+	res.send({success: false, reason: 'no user given'});
+    }
+    return next();
+});
+
 
 server.listen(port, function () {
     console.log('%s listening at %s', server.name, server.url, instance);
--- client.js
+++ client.js
@@ -1,17 +1,22 @@
 const clients = require('restify-clients');
+const crypto = require('crypto');
 
 const url = process.env.URL || 'http://localhost:3000';
 const path = process.argv[2] || '/data/test';
+//const instance = crypto.randomUUID();
+const instance = '1873e836-2fd8-4eee-a02c-35822ae99b8b';
 
 const client = clients.createJsonClient({
     url: url,
     version: '~1.0'
 });
 
+/*
 console.log('GET', url+path);
 client.get(path, (err, req, res, obj) => {
     console.log(err, obj);
 });
+*/
 
 /*
 console.log('PUT', url+path);
@@ -19,3 +24,8 @@ client.put(path, 'something from the client', (err, req, res, obj) => {
     console.log(err, obj);
 });
 */
+
+console.log('POST', url+path);
+client.post(path, JSON.stringify({user: instance, command: 'lock'}), (err, req, res, obj) => {
+    console.log(err, obj);
+});
