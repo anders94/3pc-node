3e3608375e7cbc6e977a0938d6d4a7d0acbe68f2
Anders Brownworth <anders-git@evantide.com>
2022-01-05 19:53:38 +0000

    add PUT capability if key doesn't already exist

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const restify = require('restify');
+const crypto = require('crypto');
 const db = require('./db');
 
+const instance = crypto.randomUUID();
 const port = process.env.PORT || 3000;
 
 const server = restify.createServer({
@@ -12,27 +14,42 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-server.get('/backend/:key', async (req, res, next) => {
+server.get('/data/:key', async (req, res, next) => {
+    const o = await db.get(req.params.key);
+    console.log('GET', req.params.key, o);
+    res.send(o && o.value ? o.value : '');
+    return next();
+});
+server.put('/data/:key', async (req, res, next) => {
     const o = await db.get(req.params.key);
 
-    console.log(req.params.key, o);
+    if (o) {
+	console.log('PUT', 'key already exists');
+	res.send({error: 'key already exists'});
 
-    res.send(o && o.value ? o.value : '');
+    }
+    else {
+	const o = db.put(req.params.key, {lockedBy: '', value: req.body});
+	console.log('aaa', o);
+	console.log('PUT', req.params.key, {lockedBy: '', value: req.body});
+	res.send({success: o ? true : false});
+
+    }
     return next();
 });
 
 server.listen(port, function () {
-    console.log('%s listening at %s', server.name, server.url);
+    console.log('%s listening at %s', server.name, server.url, instance);
 });
 
 const run = async () => {
     await db.init();
 
-    // upsert a key called "test" so we always have something to play with
+    // upsert a key called "test" so we have a reliable key
     let test = await db.get('test');
 
     if (!test) {
-	await db.put('test', {lockedBy: '', value: 'some data'});
+	await db.put('test', {lockedBy: '', value: instance});
 	test = await db.get('test');
     }
 
--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const clients = require('restify-clients');
 
 const url = process.env.URL || 'http://localhost:3000';
-const path = process.argv[2] || '/backend/test';
+const path = process.argv[2] || '/data/test';
 
 const client = clients.createJsonClient({
     url: url,
@@ -12,3 +12,10 @@ console.log('GET', url+path);
 client.get(path, (err, req, res, obj) => {
     console.log(err, obj);
 });
+
+/*
+console.log('PUT', url+path);
+client.put(path, 'something from the client', (err, req, res, obj) => {
+    console.log(err, obj);
+});
+*/
