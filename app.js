53418d6984567bb40fb326590644b563509b517c
Anders Brownworth <anders-git@evantide.com>
2022-01-12 12:25:31 -0500

    restify -> fastify and db -> storage

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
-const restify = require('restify');
+const fastify = require('fastify');
 const crypto = require('crypto');
-const db = require('./db');
+const storage = require('./storage');
 
 const instance = crypto.randomUUID();
 const port = process.env.PORT || 3000;
 
-const server = restify.createServer({
-    name: '3pc-node',
-    version: '0.0.1'
-});
+const server = fastify({logger: true});
 
-server.use(restify.plugins.acceptParser(server.acceptable));
-server.use(restify.plugins.queryParser());
-server.use(restify.plugins.bodyParser());
-
-server.get('/data/:key', async (req, res, next) => {
-    const o = await db.get(req.params.key);
+server.get('/data/:key', async (req, res) => {
+    const o = await storage.get(req.params.key);
     console.log('GET', req.params.key, o);
     res.send(o && o.value ? o.value : '');
-    return next();
 });
-server.put('/data/:key', async (req, res, next) => {
-    const o = await db.get(req.params.key);
+
+server.put('/data/:key', async (req, res) => {
+    const o = await storage.get(req.params.key);
 
     if (o) {
 	console.log('PUT', req.params.key, 'ERROR: Key already exists!');
@@ -29,20 +22,20 @@ server.put('/data/:key', async (req, res, next) => {
 
     }
     else {
-	const tmp = db.put(req.params.key, {lockedBy: '', value: req.body});
+	const tmp = await storage.put(req.params.key, {lockedBy: '', value: req.body});
 	console.log('PUT', req.params.key, {lockedBy: '', value: req.body});
 	res.send({success: tmp ? true : false});
 
     }
-    return next();
 });
-server.post('/data/:key', async (req, res, next) => {
-    let o = await db.get(req.params.key);
+
+server.post('/data/:key', async (req, res) => {
+    let o = await storage.get(req.params.key);
 
     if (!o) {
 	console.log('POST', req.params.key, 'ERROR: Key doesn\'t exist!');
 	res.send({error: 'Key doesn\'t exist!'});
-	return next();
+	return;
     }
 
     let obj, err;
@@ -56,14 +49,14 @@ server.post('/data/:key', async (req, res, next) => {
     if (err) {
 	console.log('POST', req.params.key, err);
 	res.send({success: false, reason: 'JSON parse error'});
-	return next();
+	return;
     }
 
     if (obj.user) {
 	if (obj.command == 'lock') {
 	    if (o.lockedBy == '' || o.lockedBy == obj.user) {
 		o.lockedBy = obj.user;
-		db.put(req.params.key, o);
+		await storage.put(req.params.key, o);
 		console.log('POST', req.params.key, obj.command, 'success');
 		res.send({success: true});
 	    }
@@ -71,6 +64,41 @@ server.post('/data/:key', async (req, res, next) => {
 		console.log('POST', req.params.key, obj.command, 'key is locked', obj);
                 res.send({success: false, reason: 'Key is already locked.'});
 	    }
+	}
+	else if (obj.command == 'unlock') {
+	    if (o.lockedBy == obj.user) {
+		o.lockedBy = '';
+		await storage.put(req.params.key, o);
+		console.log('POST', req.params.key, obj.command, 'success');
+		res.send({success: true});
+            }
+            else {
+                console.log('POST', req.params.key, obj.command, 'key is not locked by this user', obj);
+                res.send({success: false, reason: 'Key is not locked by you.'});
+            }
+	}
+	else if (obj.command == 'update') {
+	    if (o.lockedBy == obj.user) {
+		o.value = obj.value;
+		await storage.put(req.params.key, o);
+		console.log('POST', req.params.key, obj.command, obj.value, 'success');
+		res.send({success: true});
+            }
+            else {
+                console.log('POST', req.params.key, obj.command, 'key is not locked by this user', obj);
+                res.send({success: false, reason: 'Key is not locked by you.'});
+            }
+	}
+	else if (obj.command == 'delete') {
+	    if (o.lockedBy == obj.user) {
+		await storage.del(req.params.key);
+		console.log('POST', req.params.key, obj.command, 'success');
+		res.send({success: true});
+            }
+            else {
+                console.log('POST', req.params.key, obj.command, 'key is not locked by this user', obj);
+                res.send({success: false, reason: 'Key is not locked by you.'});
+            }
 	}
 	else {
 	    console.log('POST', req.params.key, obj.command, 'command unsupported');
@@ -81,25 +109,23 @@ server.post('/data/:key', async (req, res, next) => {
 	console.log('POST', req.params.key, obj.command, 'no user given');
 	res.send({success: false, reason: 'no user given'});
     }
-    return next();
 });
 
 
-server.listen(port, function () {
-    console.log('%s listening at %s', server.name, server.url, instance);
-});
+const start = async (port) => {
+    await storage.init();
 
-const run = async () => {
-    await db.init();
+    try {
+	console.log('starting on', port);
+	await server.listen(port);
 
-    // upsert a key called "test" so we have a reliable key
-    let test = await db.get('test');
+    }
+    catch (err) {
+	server.log.error(err);
+	process.exit(1);
 
-    if (!test) {
-	await db.put('test', {lockedBy: '', value: instance});
-	test = await db.get('test');
     }
 
 };
 
-run();
+start(port);
