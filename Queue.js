85f40cd894cc1b131a195334a3942f886ca8c384
Anders Brownworth <anders-git@evantide.com>
2022-01-23 22:57:17 -0500

    Queue.js: only queue functions

--- Queue.js
+++ Queue.js
@@ -1,11 +1,21 @@
+/*
+  Queue
+  =====
+
+  Queues hold an array of functions which will be executed syncronously in
+  FIFO order. New entries can be added at any time.
+*/
+
 class Queue {
     constructor() {
 	this.list = [];
 	this.running = false;
     }
     enqueue = async (f) => {
-	this.list.push(f);
-	this.run();
+	if (typeof f === 'function') {
+	    this.list.push(f);
+	    this.run();
+	}
     }
     dequeue = () => this.list.shift();
     isEmpty = () => this.list.length == 0;
