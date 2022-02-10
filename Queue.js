e9fb4580fb948eea7993fbd4968bd0f45b49e7fc
Anders Brownworth <anders-git@evantide.com>
2022-02-10 13:18:15 -0500

    Queue.js: move to a function / callback model so the queue doesn't block but you can await execution of the callback

--- Queue.js
+++ Queue.js
@@ -8,25 +8,29 @@
 
 class Queue {
     constructor() {
-	this.list = [];
-	this.running = false;
+        this.list = [];
+        this.running = false;
     }
-    enqueue = async (f) => {
-	if (typeof f === 'function') {
-	    this.list.push(f);
-	    await this.run();
+    enqueue = async (f, params, cb) => {
+	if (typeof f === 'function' &&
+	    typeof cb === 'function') {
+            this.list.push([f, params, cb]);
+            this.run();
 	}
     }
     dequeue = () => this.list.shift();
     isEmpty = () => this.list.length == 0;
     length = () => this.list.length;
     run = async () => {
-	if (!this.running) {
-	    this.running = true;
-	    while (!this.isEmpty())
-		await this.dequeue()();
-	    this.running = false;
-	}
+        if (!this.running) {
+            this.running = true;
+            while (!this.isEmpty()) {
+                const [f, params, cb] = this.dequeue();
+                await f(params);
+                cb();
+            }
+            this.running = false;
+        }
     };
 }
 
